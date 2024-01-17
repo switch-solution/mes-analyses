43386aaa65aa4def01f4576e1c00556485764d9e
Fibre44 <denis.nicolas@hotmail.fr>
2024-01-17 15:57:01 +0100

    feat : mise en place de jest avec la doc nextJs https://nextjs.org/docs/app/building-your-application/testing/jest

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
-import { getAuthSession } from "@/lib/auth"
 export default async function Home() {
-  const session = await getAuthSession()
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div> Hello world</div>
